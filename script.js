45dcfcf6d9e4e10a13de2f231afebd7cf759706b
shabarisagaa08-lgtm <shabarisagaa08@gmail.com>
2026-07-29 11:14:52 +0530

    Update script.js
    🖤 For Charik

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function nextPage(title, text) {
+    document.getElementById("app").innerHTML = `
+        <div class="screen fade">
+            <h1>${title}</h1>
+            <p>${text}</p>
+            <button class="btn" onclick="goHome()">Home</button>
+        </div>
+    `;
+}
 
+function startJourney() {
+    nextPage(
+        "🖤 Welcome",
+        "Thank you for scanning this QR code. I made something special just for you, Charik."
+    );
+}
+
+function goHome() {
+    location.reload();
+}
